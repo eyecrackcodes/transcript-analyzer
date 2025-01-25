fcee0269b2516f142afab98beb795d52d90781c4
eyecrackcodes <eyecrackcodes@gmail.com>
2025-01-25 13:04:21 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import TranscriptAnalyzer from './components/TranscriptAnalyzer';
+import React from "react";
+import TranscriptAnalyzer from "./components/TranscriptAnalyzer";
+import "./index.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <TranscriptAnalyzer />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="text-center">
+        <TranscriptAnalyzer />
+      </div>
     </div>
   );
 }
