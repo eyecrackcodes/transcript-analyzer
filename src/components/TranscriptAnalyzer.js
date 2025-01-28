be4a7f66f57f2a8e0b875c2961a7a790adb159c1
eyecrackcodes <153921539+eyecrackcodes@users.noreply.github.com>
2025-01-28 14:55:33 -0600

    Update TranscriptAnalyzer.js

--- src/components/TranscriptAnalyzer.js
+++ src/components/TranscriptAnalyzer.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import OpenAI from "openai";
 import "../index.css";
 import VoiceRecorder from './VoiceRecorder';
+import SessionController from './SessionController';
+import SessionTimer from './SessionTimer';
 
 const openai = new OpenAI({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
